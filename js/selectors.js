5d2bdb7675f0245e86a09bd857e867c1f715a904
Mohammed Ameen <jegroamin@gmail.com>
2018-09-11 23:05:04 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+function paragraphSelector(){
+  return document.querySelectorAll("p");
+}
+function lastImageSelector(){
+  return $('img:last')
+}
+function ninjaBabySelector(){
+  return document.getElementById("baby-ninja")
+}
+function divSelector(){
+  return document.getElementsByClassName("pics")
+}
+function firstListItem(){
+  return $('ul#pic-list li:first-child')
+}
