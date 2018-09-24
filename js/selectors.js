4b3548fd0f920766f97a0dba9bb655c7e846dee8
Noven Temer <noven5823@gmail.com>
2018-09-24 12:50:02 +0300

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,18 +1,29 @@
 // declare your functions here...
 // $('selector-goes-here')
-$('img');
-$('.pics');
-$('#baby-ninja');
-$('ul li');
+// $('img');
+// $('.pics');
+// $('#baby-ninja');
+// $('ul li');
 // $('parent-tag child-tag:first-child')
-$('div img:first-child');
+// $('div img:first-child');
 // $('img[alt = 'the beatles making faces']')
-$('div:last');
+// $('div:last');
 function paragraphSelector(){
-   var para = $('p');
-   for(i=0 )
-
+  return $('p')
 }
 
+function lastImageSelector(){
+  return $('img:last')
+}
 
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
 
+function divSelector(){
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('#pic-list ul:first-child')
+}
