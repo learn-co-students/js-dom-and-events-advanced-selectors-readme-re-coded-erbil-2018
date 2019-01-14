896ffa8adf375cc287d389b978a2f135c77ad300
Julian M. Qapo <julian.qapo@gmail.com>
2019-01-14 22:26:37 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+return  ${'p'}
+}
+
+function lastImageSelector(){
+  return $("img:last")
+}
+
+function ninjaBabySelector(){
+  return $('#pic-list')
+}
+
+function divSelector(){
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('li:first')
+}
